7f154722d150cee118d33f08a22470e62bd3c96b
Ray Alez <raymestalez@gmail.com>
2017-03-05 17:26:24 -0800

    Update.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
 
     url(r'^api/v1/', include('posts.urls', 
                              namespace='posts')),            
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
