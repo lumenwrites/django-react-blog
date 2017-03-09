268228a7b451990f77e8086b26bb3af71f079511
Ray Alez <raymestalez@gmail.com>
2017-03-08 20:57:23 -0800

    About page.

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchSettings } from '../actions/index';
 
-import { PageHeader, Panel } from 'react-bootstrap';
+import MetaTags from 'react-meta-tags';
+import removeMd from 'remove-markdown';
 
 import Post from './Post';
 
 
-export default class About extends Component {
+class About extends Component {
+    componentWillMount() {
+	this.props.fetchSettings();	
+    }
+
+    renderMetaInfo () {
+	const settings =  this.props.settings;
+	const post = this.props.post;
+
+	if (!settings) { return null; }
+	/* Remove markdown from post body, and truncate it to 160 chars. */
+
+	var body = ""
+	if (settings.about) {
+	    body = removeMd(settings.about);
+	}
+	const truncate_length = 160;
+	const description = body.substring(0, truncate_length - 3) + "...";
+
+	return (
+            <MetaTags>
+		<title>{"About " + settings.title}</title>
+		<meta name="author" content={settings.author} />  
+		<meta name="description"
+		      content={description} />
+		<meta name="keywords"
+		      content={settings.keywords} />		
+            </MetaTags>
+	);
+    }
+
+    
     render() {
+	var about =  this.props.settings.about;
+	if (!about) {
+	    about = "To edit this text, go to /admin, create settings object, and fill in the info."
+	}
 	return (
 	    <div>
-		<Post title="About" body="Welcome to my blog!!"/>	
+		{ this.renderMetaInfo() }
+		<Post title="About" body={about}/>
 	    </div>	    
 	);
     }
 }
 
 
+function mapStateToProps(state) {
+    return { settings: state.settings.all };
+}
+
+export default connect(mapStateToProps, { fetchSettings })(About);
