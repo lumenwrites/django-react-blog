31b0a36a1fd341f037e47dc17e9a292e620954ec
Ray Alez <raymestalez@gmail.com>
2017-03-09 00:59:22 -0800

    Update.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,9 +6,7 @@ export default class Footer extends Component {
     render() {
 	return (
 	    <footer className="footer">
-		<FontAwesome name='rocket' />
 		<div className="right credit">
-
 		    Made by <a href="http://rayalez.com">Ray Alez</a>
 		    <a href="https://github.com/raymestalez/django-react-blog">
 			<FontAwesome name='github' />
