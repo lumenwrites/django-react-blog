ab3ba1342545bd08b66dd0c7c4f0ad9bbca4b03c
Ray Alez <raymestalez@gmail.com>
2017-03-09 00:53:28 -0800

    Fontawesome github icon.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-
+import FontAwesome from 'react-fontawesome';
 
 
 export default class Footer extends Component {
     render() {
 	return (
 	    <footer className="footer">
+		<FontAwesome name='rocket' />
 		<div className="right credit">
+
 		    Made by <a href="http://rayalez.com">Ray Alez</a>
 		    <a href="https://github.com/raymestalez/django-react-blog">
-			<i className="fa fa-github"></i>
+			<FontAwesome name='github' />
 		    </a>
 		</div>
 	    </footer>
