99b693e17d336561b611c40900e474c1791a467d
Ray Alez <raymestalez@gmail.com>
2017-03-14 01:35:28 -0700

    Update.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ["*",]
 
 
 # Application definition
